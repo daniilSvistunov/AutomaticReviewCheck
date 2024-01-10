9e5866f1bee182119edef0d15781c7e63bc4631c
Jan-Niklas Karger <jkg@objektkultur.de>
2024-01-10 10:53:21 +0100

    Removed not needed mock data, renamed project to OKTemplate

--- Frontend/src/api/mock/feature.ts
+++
@@ -1,8 +0,0 @@
-
-import { mock } from './mockAdapter';
-
-// Wichtig: Die Reihenfolge der Mocks ist wichtig, da sonst die spezifischeren Mocks nicht greifen.
-// Also erst die spezifischen Mocks, dann die allgemeinen.
-
-// GET
-mock.onGet(new RegExp(`${import.meta.env.VITE_ENDPOINT_EMPLOYEES}/\\w+`)).reply(200); // TODO return
