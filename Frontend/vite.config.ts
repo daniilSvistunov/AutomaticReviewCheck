ee1983d19b9ed119ebb45423bb5ecbb39f24b4c5
Pascal Heck <phe@objektkultur.de>
2024-05-03 08:47:04 +0200

    ignoring stories for coverage report

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -4,72 +4,71 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-export default defineConfig(
-  defineConfig({
-    plugins: [
-      react(),
-      basicSsl(),
-      federation({
-        name: 'Auftragsverwaltung',
-        filename: 'remoteEntry.js',
-        exposes: {
-          './AuftragsverwaltungApp': './src/App',
-        },
-        shared: [
-          'react',
-          'react-dom',
-          '@mui/lab',
-          '@mui/material',
-          '@mui/system',
-          'react-router-dom',
-          'redux',
-          'react-redux',
-          'redux-persist',
-          '@reduxjs/toolkit',
-          '@azure/msal-react',
-          '@azure/msal-browser',
-        ],
-      }),
-    ],
-    resolve: {
-      alias: {
-        '@components': path.resolve(__dirname, './src/components'),
-        '@models': path.resolve(__dirname, './src/models'),
-        '@pages': path.resolve(__dirname, './src/pages'),
-        '@locales': path.resolve(__dirname, './src/locales'),
-        '@routes': path.resolve(__dirname, './src/routes'),
-        '@redux': path.resolve(__dirname, './src/redux'),
-        '@utils': path.resolve(__dirname, './src/utils'),
-        '@layouts': path.resolve(__dirname, './src/layouts'),
-        '@hooks': path.resolve(__dirname, './src/hooks'),
-        '@api': path.resolve(__dirname, './src/api'),
-        '@sections': path.resolve(__dirname, './src/sections'),
+export default defineConfig({
+  plugins: [
+    react(),
+    basicSsl(),
+    federation({
+      name: 'Auftragsverwaltung',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './AuftragsverwaltungApp': './src/App',
       },
+      shared: [
+        'react',
+        'react-dom',
+        '@mui/lab',
+        '@mui/material',
+        '@mui/system',
+        'react-router-dom',
+        'redux',
+        'react-redux',
+        'redux-persist',
+        '@reduxjs/toolkit',
+        '@azure/msal-react',
+        '@azure/msal-browser',
+      ],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@models': path.resolve(__dirname, './src/models'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@locales': path.resolve(__dirname, './src/locales'),
+      '@routes': path.resolve(__dirname, './src/routes'),
+      '@redux': path.resolve(__dirname, './src/redux'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@api': path.resolve(__dirname, './src/api'),
+      '@sections': path.resolve(__dirname, './src/sections'),
     },
-    envDir: './src/env',
-    server: {
-      open: true,
-      host: 'localhost',
-      port: 3005, // TODO: needs to be changed for new application
+  },
+  envDir: './src/env',
+  server: {
+    open: true,
+    host: 'localhost',
+    port: 3005, // TODO: needs to be changed for new application
+  },
+  build: {
+    outDir: './build',
+    target: 'esnext',
+    cssCodeSplit: false,
+    minify: false,
+    modulePreload: false,
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./tests/setup.ts'],
+
+    globals: true,
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      exclude: ['**/*.stories.tsx', '**/.storybook/*' ],
     },
-    build: {
-      outDir: './build',
-      target: 'esnext',
-      cssCodeSplit: false,
-      minify: false,
-      modulePreload: false,
-    },
-    test: {
-      environment: 'jsdom',
-      setupFiles: ['./tests/setup.ts'],
-  
-      globals: true,
-      coverage: {
-        reporter: ['text', 'json', 'html'],
-      },
-    },
-    define: {
-      APP_VERSION: JSON.stringify(process.env.npm_package_version),
-    },
-  })
-);
+  },
+  define: {
+    APP_VERSION: JSON.stringify(process.env.npm_package_version),
+  },
+});
