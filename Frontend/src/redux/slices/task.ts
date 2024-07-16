0e57ff2c5171df7bfdb100ec80a3ea9a8db0d671
Piet Maier <pma@objektkultur.de>
2024-07-16 16:14:36 +0200

    FEAT: Add Toggle & Remove Actions

--- Frontend/src/redux/slices/task.ts
+++ Frontend/src/redux/slices/task.ts
@@ -1,9 +1,12 @@
+import { type RootState } from '@redux/store';
 import { type PayloadAction, createSlice } from '@reduxjs/toolkit';
 
 // ----------------------------------------------------------------------
 
-interface Task {
+export interface Task {
+  ID: number;
   title: string;
+  checked: boolean;
 }
 
 interface TaskState {
@@ -20,14 +23,32 @@ export const taskSlice = createSlice({
   name: 'task',
   initialState,
   reducers: {
-    create: (state, action: PayloadAction<Task>) => {
-      state.list.push(action.payload);
+    create: (state, action: PayloadAction<Omit<Task, 'ID' | 'checked'>>) => {
+      state.list.push({
+        ID: state.list.length === 0 ? 1 : state.list[state.list.length - 1].ID + 1,
+        checked: false,
+        ...action.payload,
+      });
+    },
+    toggle: (state, action: PayloadAction<number>) => {
+      const index = state.list.findIndex((task) => task.ID === action.payload);
+
+      state.list[index].checked = !state.list[index].checked;
+    },
+    remove: (state, action: PayloadAction<number>) => {
+      const index = state.list.findIndex((task) => task.ID === action.payload);
+
+      state.list.splice(index, 1);
     },
   },
 });
 
 // ----------------------------------------------------------------------
 
-export const { create } = taskSlice.actions;
+export const selectList = (state: RootState) => state.task.list;
+
+// ----------------------------------------------------------------------
+
+export const { create, toggle, remove } = taskSlice.actions;
 
 export default taskSlice.reducer;
