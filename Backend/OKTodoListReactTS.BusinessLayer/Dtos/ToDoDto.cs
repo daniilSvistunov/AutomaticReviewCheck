ff5b870df9fc413e497dcb22f05270e28acf5ba0
Lukas Finna <lfi@objektkultur.de>
2024-08-01 12:50:10 +0200

    Add field title to dto and entry

--- Backend/OKTodoListReactTS.BusinessLayer/Dtos/ToDoDto.cs
+++ Backend/OKTodoListReactTS.BusinessLayer/Dtos/ToDoDto.cs
@@ -6,6 +6,8 @@ namespace OKTodoListReactTS.BusinessLayer.Dtos
     {
         public Guid Id { get; set; }
 
+        public string Title { get; set; }
+
         public string Text { get; set; }
 
         public DateTime DueDate { get; set; }
