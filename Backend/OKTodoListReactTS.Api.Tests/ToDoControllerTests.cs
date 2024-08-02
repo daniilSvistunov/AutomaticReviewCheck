66c63f4f88df557b54ef4b4771a27fd8efc3c33b
Lukas Finna <lfi@objektkultur.de>
2024-08-02 14:51:41 +0200

    Implement get all controller test

--- Backend/OKTodoListReactTS.Api.Tests/ToDoControllerTests.cs
+++ Backend/OKTodoListReactTS.Api.Tests/ToDoControllerTests.cs
@@ -1,15 +1,61 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using OKTodoListReactTS.Api.Controllers;
+using OKTodoListReactTS.BusinessLayer.Dtos;
 using OKTodoListReactTS.BusinessLayer.Interfaces;
 using Xunit;
 
 namespace OKTodoListReactTS.Api.Tests
 {
+    /// <summary>
+    /// Runs tests on <see cref="ToDoController"/>.
+    /// </summary>
     public class ToDoControllerTests
     {
-        private readonly Mock<IToDoService> _toDoServiceMock = new();
-        private ToDoController _toDoController;
+        private static readonly Random _random = new();
+        private readonly Mock<IToDoService> _serviceMock = new();
+        private readonly ToDoController _controller;
+
+        private readonly List<ToDoDto> _elements = Enumerable
+            .Range(1, _random.Next(10, 20))
+            .Select(num => new ToDoDto()
+            {
+                Id = Guid.NewGuid(),
+                Title = $"Title {num}",
+            }).ToList();
+
+        public ToDoControllerTests()
+        {
+            _controller = new ToDoController(_serviceMock.Object);
+        }
+
+        [Fact]
+        public async Task GetAllToDosAsync_ReturnsStatusCodeOk()
+        {
+            // Arrange
+            _serviceMock.Setup(s => s.GetAllTodosAsync()).ReturnsAsync(_elements);
+
+            // Act
+            var controllerResult = await _controller.GetAllTodosAsync();
+
+            // Assert
+            Assert.NotNull(controllerResult);
+
+            var payload = controllerResult.Result;
+            Assert.IsType<OkObjectResult>(payload);
+
+            var value = (payload as OkObjectResult)?.Value;
+            var statusCode = (value as OkObjectResult)?.StatusCode;
+            Assert.NotNull(value);
+            Assert.NotNull(statusCode);
+            Assert.Equal(200, statusCode);
+
+            _serviceMock.Verify(s => s.GetAllTodosAsync(), Times.Once);
+        }
 
         [Fact]
         public async Task AddToDoAsync_ReturnsStatusCodeOk()
@@ -17,10 +63,8 @@ namespace OKTodoListReactTS.Api.Tests
             // Arrange
             // TODO: Setup
 
-            _toDoController = new ToDoController(_toDoServiceMock.Object);
-
             // Act
-            var result = await _toDoController.AddToDoAsync(null/* TODO */);
+            var result = await _controller.AddToDoAsync(null/* TODO */);
 
             // Assert
             // TODO: add assertion
